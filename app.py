9e2003d39f4aa497a14f33dd918b5d54fafb1d1f
Oshadini Kariyawasam <43576871+Oshadini@users.noreply.github.com>
2024-12-10 02:01:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ if uploaded_file:
                 Feedback(standalone.custom_metric_score)
                 .on(answer=Select.RecordOutput)
                 .on(question=Select.RecordInput)
-                .on(context=Select.Literal(context))  # Wrap context in a Lens
+                .on(context)  # Wrap context in a Lens
             )
         
             # Simulate chain and record evaluation
