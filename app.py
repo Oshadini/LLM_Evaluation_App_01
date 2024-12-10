502906b546b1397457c7b11c29cebfc6d3daad52
Oshadini Kariyawasam <43576871+Oshadini@users.noreply.github.com>
2024-12-10 17:24:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ if uploaded_file:
 
             # Step 3: Let the user select columns
             selected_columns = st.multiselect(
-                "Select columns used in the prompt:",
+                "Select columns to be evaluated:",
                 options=required_columns,
                 #default=["Question", "Answer"]
             )
